c04827fa5de9c6aa1b4869908b3fa089d2cdec65
Md. Humayoun Kabir <humayounkabir30@gmail.com>
2019-01-02 17:57:28 +0600

    Add files via upload

---
+++ NotifierApp/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace NotifierApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "Exit";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+
+    }
+}
+
---
+++ NotifierApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotifierApp
+{
+    public partial class Form1 : Form
+    {       
+     
+
+        public Form1()
+        {
+            InitializeComponent();          
+            
+        }
+
+        protected void Displaynotify()
+        {
+            try
+            {                
+                notifyIcon1.Icon = new System.Drawing.Icon(Path.GetFullPath(@"image\notification.ico"));
+                notifyIcon1.Text = "Warning about eye protection";
+                notifyIcon1.Visible = true;
+                notifyIcon1.BalloonTipTitle = "After every 20 minutes Skip monitor for about 3 minutes ";
+                notifyIcon1.BalloonTipText = "Warning about eye protection";
+                notifyIcon1.ShowBalloonTip(2000);
+            }
+            catch (Exception ex)
+            {
+            }
+        }  
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer gt = new Timer();
+            gt.Interval = 1000 * 60 * 20;//for 20 seconds
+            //gt.Interval = 20000;
+            gt.Tick += new EventHandler(gt_Tick);            
+           
+            gt.Start();
+            
+        }
+
+        void gt_Tick(object sender, EventArgs e)
+        {
+            Displaynotify();
+        }     
+
+       
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //to minimize window
+            this.WindowState = FormWindowState.Minimized;
+
+            //to hide from taskbar
+            this.Hide();
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+
+        
+    }
+}
